724a8dc9ee29619b743fb5787cec10dba5ffec46
Shahriyor <shahriyordeveloping@gmail.com>
2021-08-15 18:09:57 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -19,3 +19,42 @@ window.onclick = (e: MouseEvent) => {
   )
     menuToggle();
 };
+
+
+const btn = document.querySelector('.calendar__month-btn');
+const ac = document.querySelectorAll('.calendar__vis-p');
+const nextBtn = document.querySelector('.calendar__month-next');
+const prevBtn = document.querySelector('.calendar__month-prev');
+function openMonth(el = btn){
+  el.classList.toggle('open');
+}
+function addActive(el){
+  for(let i = 0; i < ac.length; i++){
+    ac[i].classList.remove('active');
+  }
+  el.classList.add('active');
+  btn.innerHTML = el.innerHTML;
+  setTimeout(() => {
+    openMonth()
+  }, 10);
+}
+function next(el){
+  for(let i = 0; i < ac.length; i++){
+    if(ac[i].classList.contains('active')){
+      console.log('ac');
+      ac[i+1].classList.add('active');
+      btn.innerHTML = ac[i+1].innerHTML;
+      ac[i].classList.remove('active');
+    }
+  }
+}
+function prev(el){
+  for(let i = 0; i < ac.length; i++){
+    if(ac[i].classList.contains('active')){
+      console.log('ac');
+      ac[i-1].classList.add('active');
+      btn.innerHTML = ac[i-1].innerHTML;
+      ac[i].classList.remove('active');
+    }
+  }
+}
